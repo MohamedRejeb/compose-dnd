193faed7bce639a95bd2052fc60ae0140c22fe45
MohamedRejeb <mohamedrejeb445@gmail.com>
2026-04-04 18:26:32 +0100

    chore: migrate to vanniktech maven-publish and add renovate configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    id("root.publication")
+    alias(libs.plugins.vanniktech.maven.publish) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.spotless) apply false
+}
+
+val composeDndVersion = System.getenv("VERSION") ?: "0.5.0-SNAPSHOT"
+
+allprojects {
+    group = "com.mohamedrejeb.dnd"
+    version = composeDndVersion
 }
--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.nexus.publish)
+    implementation(libs.vanniktech.maven.publish)
     implementation(libs.spotless.gradle)
     // hack to access version catalogue https://github.com/gradle/gradle/issues/15383
     compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
