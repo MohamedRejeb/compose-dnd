2d520fe859ae23ec585947d7d9ccf54833f44316
MohamedRejeb <mohamedrejeb445@gmail.com>
2026-07-08 13:21:23 +0100

    sample: remove unused Compose imports

--- sample/desktop/src/desktopMain/kotlin/main.kt
+++ sample/desktop/src/desktopMain/kotlin/main.kt
@@ -1,5 +1,3 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
